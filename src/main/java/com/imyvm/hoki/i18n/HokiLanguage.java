7eacb70558c71111b216bdc1e5688c30a8111f64
YanWQ-monad <YanWQmonad@gmail.com>
2022-07-02 13:18:52 +0800

    refactor(i18n): add some documentation annotations

--- src/main/java/com/imyvm/hoki/i18n/HokiLanguage.java
+++ src/main/java/com/imyvm/hoki/i18n/HokiLanguage.java
@@ -3,6 +3,8 @@ package com.imyvm.hoki.i18n;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -11,12 +13,28 @@ import java.util.Objects;
 
 public abstract class HokiLanguage {
     private static final Gson GSON = new Gson();
+    private static final HokiLanguage EMPTY = new HokiLanguage() {
+        @Override
+        public @NotNull String get(@NotNull String key) {
+            return key;
+        }
 
-    public static String getResourcePath(String modId, String languageId) {
+        @Override
+        public boolean hasTranslation(@NotNull String key) {
+            return false;
+        }
+    };
+
+    @NotNull
+    public static String getResourcePath(@NotNull String modId, @NotNull String languageId) {
         return String.format("/assets/%s/lang/%s.json", modId, languageId);
     }
 
-    public static HokiLanguage create(InputStream inputStream) {
+    @NotNull
+    public static HokiLanguage create(@Nullable InputStream inputStream) {
+        if (inputStream == null)
+            return EMPTY;
+
         ImmutableMap.Builder<String, String> builder = ImmutableMap.builder();
 
         JsonObject json = GSON.fromJson(new InputStreamReader(inputStream, StandardCharsets.UTF_8), JsonObject.class);
@@ -25,18 +43,20 @@ public abstract class HokiLanguage {
         ImmutableMap<String, String> map = builder.build();
         return new HokiLanguage() {
             @Override
-            public String get(String key) {
+            @NotNull
+            public String get(@NotNull String key) {
                 return Objects.requireNonNullElse(map.get(key), key);
             }
 
             @Override
-            public boolean hasTranslation(String key) {
+            public boolean hasTranslation(@NotNull String key) {
                 return map.containsKey(key);
             }
         };
     }
 
-    public abstract String get(String key);
+    @NotNull
+    public abstract String get(@NotNull String key);
 
-    public abstract boolean hasTranslation(String key);
+    public abstract boolean hasTranslation(@NotNull String key);
 }
